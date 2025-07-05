1a1ff4b09647c57ffeaf69d0fa6fee31dfe11ab6
aymane elouafi <133535379+aymane-elouafi@users.noreply.github.com>
2025-07-05 12:55:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import ui 
+import ui
 import ciphers
 
 
-def main() :
+def main():
     ui.clear_screen()
-    while True :
+    while True:
         ui.header()
         ui.display_main_menu()
         cipher = input("    > ").strip()
-        if cipher == "1" :
+        if cipher == "1":
             ui.clear_screen()
             ui.header()
             ui.display_cipher_selection_menu()
             mode = input("> ").strip()
             ciphers.caesar_cipher(mode)
-        elif cipher == "2" :
+        elif cipher == "2":
             ui.clear_screen()
             ui.header()
             ui.display_cipher_selection_menu()
             mode = input("> ").strip()
             ciphers.vigenere_cipher(mode)
-        elif cipher == "q" :
+        elif cipher == "q":
             ui.clear_screen()
             return
 
-            
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()
