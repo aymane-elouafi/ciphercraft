ba449f08b0258e126e9ac06916f5ce01aaca504d
aymane elouafi <133535379+aymane-elouafi@users.noreply.github.com>
2025-07-05 12:56:49 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,62 +1,61 @@
-import os 
+import os
 import platform
 
 
-class Colors :
-    # class attribute 
+class Colors:
+    # class attribute
     SUCCESS = "\033[92m"
     ERROR = "\033[91m"
     INFO = "\033[96m"
     RESET = "\033[0m"
 
 
-def print_success(message) :
+def print_success(message):
     print(f"{Colors.SUCCESS}{message}{Colors.RESET}")
-def print_error(message) :
+
+
+def print_error(message):
     print(f"{Colors.ERROR}{message}{Colors.RESET}")
-def print_info(message) :
+
+
+def print_info(message):
     print(f"{Colors.INFO}{message}{Colors.RESET}")
 
 
-def header() :
+def header():
     print_success("""
  ▄▄· ▪   ▄▄▄· ▄ .▄▄▄▄ .▄▄▄   ▄▄· ▄▄▄   ▄▄▄· ·▄▄▄▄▄▄▄▄
-▐█ ▌▪██ ▐█ ▄███▪▐█▀▄.▀·▀▄ █·▐█ ▌▪▀▄ █·▐█ ▀█ ▐▄▄·•██  
+▐█ ▌▪██ ▐█ ▄███▪▐█▀▄.▀·▀▄ █·▐█ ▌▪▀▄ █·▐█ ▀█ ▐▄▄·•██
 ██ ▄▄▐█· ██▀·██▀▐█▐▀▀▪▄▐▀▀▄ ██ ▄▄▐▀▀▄ ▄█▀▀█ ██▪  ▐█.▪
 ▐███▌▐█▌▐█▪·•██▌▐▀▐█▄▄▌▐█•█▌▐███▌▐█•█▌▐█ ▪▐▌██▌. ▐█▌·
-·▀▀▀ ▀▀▀.▀   ▀▀▀ · ▀▀▀ .▀  ▀·▀▀▀ .▀  ▀ ▀  ▀ ▀▀▀  ▀▀▀               
+·▀▀▀ ▀▀▀.▀   ▀▀▀ · ▀▀▀ .▀  ▀·▀▀▀ .▀  ▀ ▀  ▀ ▀▀▀  ▀▀▀
      Command-line tool for classical cryptography
-_____________________________________________________               
+_____________________________________________________
 """)
 
 
-def clear_screen() :
-    if platform.system() == "Windows" :
+def clear_screen():
+    if platform.system() == "Windows":
         os.system("cls")
-    else : 
+    else:
         os.system("clear")
 
 
-def display_main_menu() :
+def display_main_menu():
     clear_screen()
     header()
     print_info("""
     [1] Caesar Cipher
     [2] Vigenère Cipher
 
-    [q] Quit   
+    [q] Quit
                """)
 
 
-def display_cipher_selection_menu() :
+def display_cipher_selection_menu():
     print_info("""
     [1] Encrypt
     [2] Decrypt
 
-    [c] Cancel 
+    [c] Cancel
                """)
-
-
-
-
-    
